79b989248372d61546112eaf3327fce6a8b181b3
mackie <macklinu@users.noreply.github.com>
2023-10-08 19:45:55 -0400

    Setup migrations and resolve build errors

---
+++ src/db/migrate.ts
@@ -0,0 +1,9 @@
+import { migrate } from 'drizzle-orm/node-postgres/migrator'
+
+import { db } from './client'
+
+async function main() {
+  await migrate(db, { migrationsFolder: 'drizzle' })
+}
+
+main()
