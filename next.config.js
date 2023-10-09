b1cde5bc475fa519d50949d4d6ed7cc060e01c76
mackie <macklinu@users.noreply.github.com>
2023-10-09 06:47:57 -0400

    Add root route cache control headers

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: async () => [
+    {
+      source: '/',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 's-maxage=86400, stale-while-revalidate=3600',
+        },
+      ],
+    },
+  ],
   images: {
     domains: ['detroittitans.com'],
   },
