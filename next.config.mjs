cfc82805959596da5f5a04f5edcec0e65f53fddb
mackie <macklinu@users.noreply.github.com>
2023-10-10 15:20:42 -0400

    Transpile libsql/client

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: 'loose',
-  },
+  transpilePackages: ['@libsql/client', '@libsql/isomorphic-fetch'],
   images: {
     domains: ['detroittitans.com'],
   },
