57c3c6523a2355a2acda8f3956ae65899f910652
mackie <macklinu@users.noreply.github.com>
2023-10-10 14:32:52 -0400

    Fix schema migrations and checks during build

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,11 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  driver: 'turso',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN!,
   },
+  verbose: true,
+  strict: true,
 } satisfies Config
